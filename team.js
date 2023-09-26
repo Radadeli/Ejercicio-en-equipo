df2569b5ae5fd56a77f853f76fccbff1c3f3b8ca
SrWilly19 <wahc1998@gmail.com>
2023-09-26 09:29:05 +0200

    Completado Willy

--- team.js
+++ team.js
@@ -10,4 +10,15 @@ const members = [
         favoriteBook: "",
         petName: "",
     }
+    {   firstname: "Willy",
+        surname: "Hidalgo",
+        age: "25",
+        city: "Valencia",
+        hobby: "Leer comics",
+        favoriteFood: "Postres",
+        favoriteVideoGame: "CoD",
+        favoriteFilm: "Batman",
+        favoriteBook: "Noches oscuras",
+        petName: "Buggy",
+    }
 ]
