2e95a7a9a9d20f029c0fbb1add38d6e492cf7525
Ramiro De Lillo <145443651+Radadeli@users.noreply.github.com>
2023-09-28 11:28:45 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,70 +1,5 @@
 const members = [
-<<<<<<< HEAD
-  {
 
-    firstname: " ramiro",
-    surname: "de lillo",
-    age: "28",
-    city: "Azuaga",
-    hobby: "Play Football",
-    favoriteFood: "Asado",
-    favoriteVideoGame: "Fifa",
-    favoriteFilm: "Prison Break",
-    favoriteBook: "Desarrole su cerebro",
-    petName: "Zoe",
-  },
-  {
-
-    firstname: "",
-    surname: "",
-    age: "",
-    city: "",
-    hobby: "",
-    favoriteFood: "",
-    favoriteVideoGame: "",
-    favoriteFilm: "",
-    favoriteBook: "",
-    petName: "",
-  },
-  {
-    firstname: "Luis",
-    surname: "Tortosa",
-    age: "25",
-    city: "Salamanca",
-    hobby: "Programar",
-    favoriteFood: "Cocido madrileño",
-    favoriteVideoGame: "Fallout New Vegas",
-    favoriteFilm: "Interestellar",
-    favoriteBook: "Fight Club",
-    petName: "El Picos",
-  },
-
-  {
-    firstname: "Manuel",
-    surname: "Benitez",
-    age: "29",
-    city: "Villa Carlos Paz",
-    hobby: "Programar",
-    favoriteFood: "Milanesa",
-    favoriteVideoGame: "DOTA 2",
-    favoriteFilm: "",
-    favoriteBook: "Shantaram",
-    petName: "",
-  },
-  {
-    firstname: "Willy",
-    surname: "Hidalgo",
-    age: "25",
-    city: "Valencia",
-    hobby: "Leer comics",
-    favoriteFood: "Postres",
-    favoriteVideoGame: "CoD",
-    favoriteFilm: "Batman",
-    favoriteBook: "Noches oscuras",
-    petName: "Buggy",
-  },
-
-=======
     {   firstname: "",
         surname: "",
         age: "",
@@ -135,5 +70,4 @@ const members = [
       favoriteBook: "Desarrole su cerebro",
       petName: "Zoe",
     },
->>>>>>> d2640f5589df8e61a46581018221ef15ac06ba2a
 ];
