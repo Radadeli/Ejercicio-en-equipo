61179667cbab17d07e044fdced706be6e6b1bc2f
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-09-29 12:08:59 +0200

    Feat
    No working yet
    
    Submitting changes in order to work on it later

--- team.js
+++ team.js
@@ -80,3 +80,41 @@ const members = [
       petName: "Zoe",
     },
 ];
+
+function sameAge(members) {
+    /*for (let i = 0; i < members.length; i++) {
+        const element = members[i];
+        let y= i+1;
+        const nextElement= members[y];
+        if (element.age==nextElement.age) {
+            console.log(element.firstname + " has " + element.age+" as " +nextElement.firstname);
+        } else {
+            console.log("Keep doing comparison");
+            console.log(element.firstname,element.age);
+        }
+        
+    }
+    *First idea of code. 
+    *Found better way to do it.
+    *Didn't erase in order to 
+    *let it readeable for team.
+    */
+   let result= "";
+    for (let i = 0; i < members.length; i++) {
+        const element = members[i];
+        let hasAppeared= false;
+        while (hasAppeared==false) {
+            for (let i = 0; i < members.length; i++) {
+                const nextElement= members[i];
+                if (element.firstname==nextElement.firstname & element.surname!=nextElement.surname) {
+                    console.log(element.firstname+" "+element.surname+ " tiene el mismo nombre que " + nextElement.firstname+" "+element.surname);
+                    hasAppeared=true;
+                }else{
+                    console.log("No hay nadie con el mismo nombre que "+element.firstname);
+                }
+            }    
+        }  
+    }
+}
+
+sameAge(members);
